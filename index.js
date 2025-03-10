6ef04ab541cd494192e7f231832b92e410c69cd7
Angel Machicado <angelmachicado40@gmail.com>
2025-03-10 10:43:29 -0400

    PROYECTO_C

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const pool=require('./database');
-async function main() {
-    const connection=await pool.getConnection();
-    try{
-        const[row]=await connection.query('SELECT * FROM estudiante');
-        console.log('Datos de usuario',row);
+const pool = require('./database');
 
-        const resultado= await connection.query('INSERT INTO estudiante(nombre,apellidoPaterno,apellidoMaterno,correo,Ci) VALUES(?,?,?,?,?)',['Mario','Mamani','Soliz','MArioM@gmail.com','4344443434']);
-        console.log('Id del nuevo Registro: ',resultado[0].insertId);
-    }catch(error){
-        console.error('Error en la consulta',error);
-    }finally{
+
+
+async function main() {
+    const connection = await pool.getConnection();
+    try {
+        // Mostrar todos los estudiantes
+        const [rows] = await connection.query('SELECT * FROM estudiante');
+        console.log('Datos de estudiantes:', rows);
+
+        
+        const resultado = await connection.query(
+            'INSERT INTO estudiante(nombre, apellidoPaterno, apellidoMaterno, correo, Ci) VALUES(?, ?, ?, ?, ?)',
+            ['Mario', 'Mamani', 'Soliz', 'MarioM@gmail.com', '4344443434']
+        );
+        console.log('Id del nuevo Registro: ', resultado[0].insertId);
+        
+    } catch (error) {
+        console.error('Error en la consulta', error);
+    } finally {
         connection.release();
     }
 }
+
 main();
